8c8a95c6a6141ee2c2ce820c1f936c9fd823c7f3
Rachel Hill <hillrachel187@gmail.com>
2022-03-21 21:30:11 -0600

    Add changes to Turn test suite

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -48,10 +48,17 @@ describe('Turn', function() {
     expect(turn.evaluateGuess()).to.equal(false);
   });
 
-  it('should give feedback whether the guess is correct or not', function() {
+  it('should give feedback if the guess is incorrect', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const turn = new Turn('array', card);
 
     expect(turn.giveFeedback()).to.equal('incorrect!');
+  });
+
+  it('should give feedback if the guess is correct', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+
+    expect(turn.giveFeedback()).to.equal('correct!');
   });
 });
